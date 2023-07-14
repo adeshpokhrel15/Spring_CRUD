d9c3a9065d7579dc581fdb31b013a197588c9f5a
adeshpokhrel <apokhrel@inventechgroup.com>
2023-07-15 03:17:19 +0545

    save multiple  data loaded into database

--- src/main/java/com/cruddemo/demo/DemoApplication.java
+++ src/main/java/com/cruddemo/demo/DemoApplication.java
@@ -20,7 +20,9 @@ public class DemoApplication {
 		//executed after the spring beans have been loaded
 		return runner-> {
 			System.out.println("Hello World");
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+
+			createMultipleStudent(studentDAO);
 		};
 	}
 
@@ -29,8 +31,8 @@ public class DemoApplication {
 		// create the student object
 		System.out.println("Creating new student objects.....");
 		Student tempStudent = new Student(
-				"Adesh",
-				"Pokhrel",
+				"Hello",
+				"Buddy",
 				"adeshpokhrel15@gmail.com"
 
 		);
@@ -43,6 +45,47 @@ public class DemoApplication {
 
 		//display id of the saved student
 		System.out.println("Saveed student. Generate id:" + tempStudent.getId());
+
+	}
+	private void createMultipleStudent(StudentDAO studentDAO){
+
+		// create the student object
+		System.out.println("Creating new student objects.....");
+		Student tempStudent1 = new Student(
+				"Hello",
+				"Buddy",
+				"adeshpokhrel15@gmail.com"
+
+		);
+		Student tempStudent2 = new Student(
+				"Buddy",
+				"Buddy",
+				"adeshpokhrel15@gmail.com"
+
+		);
+		Student tempStudent3 = new Student(
+				"Buddy",
+				"Hello",
+				"adeshpokhrel15@gmail.com"
+
+		);
+		System.out.println(tempStudent1);
+		System.out.println(tempStudent2);
+		System.out.println(tempStudent3);
+
+		// save the student object
+		System.out.println("Saving the student");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent3);
+		studentDAO.save(tempStudent3);
+
+
+
+		//display id of the saved student
+		System.out.println("Saveed student. Generate id:" + tempStudent1.getId());
+		System.out.println("Saveed student. Generate id:" + tempStudent2.getId());
+		System.out.println("Saveed student. Generate id:" + tempStudent3.getId());
+
 
 	}
 
